30b8f060d262e9e5f77f0adf04c62aeef92583f0
Vitalii <seminenkovs@gmail.com>
2023-12-18 22:33:42 +0100

    Created LoginViewModel

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels;
+
+public class LoginViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Display(Name="Remember Me?")]
+    public bool RememberMe { get; set;}
+
+    public string ReturnUrl { get; set;}
+}

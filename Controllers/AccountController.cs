9446f0ac665f61447fee8845ec06b666686ff554
Vitalii <seminenkovs@gmail.com>
2023-12-18 18:59:44 +0100

    AccountController created post method Register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using IdentityApp.ViewModels;
+﻿using IdentityApp.Models;
+using IdentityApp.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,11 +21,33 @@ namespace IdentityApp.Controllers
             return View();
         }
 
+        [HttpGet]
         public async Task<IActionResult> Register(string returnUrl = null)
         {
             RegisterViewModel registerViewModel = new RegisterViewModel();
             registerViewModel.ReturnUrl = returnUrl;
             return View(registerViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel, string returnUtl)
+        {
+            registerViewModel.ReturnUrl = returnUtl;
+            returnUtl = returnUtl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { Email = registerViewModel.Email };
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUtl);
+                }
+                ModelState.AddModelError("Password", "User could not be created. Password is not unique enough");
+            }
+
+            return View(registerViewModel);
+
         }
     }
 }

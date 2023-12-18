7401057544ac8dd8e3b23d008f3acb4cd41020ee
Vitalii <seminenkovs@gmail.com>
2023-12-18 18:12:37 +0100

    Created AccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

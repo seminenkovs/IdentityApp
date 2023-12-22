0ab615bc016881eab56517b5d688a72c1ceaaaee
Vitalii <seminenkovs@gmail.com>
2023-12-22 22:39:02 +0100

    Created ExternalLoginViewModel

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -5,7 +5,7 @@ namespace IdentityApp.ViewModels;
 public class LoginViewModel
 {
     [Required]
-    public string UserName { get; set; }
+    public string Email { get; set; }
 
     [Required]
     [DataType(DataType.Password)]

1bc52fcae442bd07a3ecd173b7ab78a8d6c8e059
Vitalii <seminenkovs@gmail.com>
2023-12-27 03:32:06 +0100

    UserConteroller added Edit method and View

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using IdentityApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace IdentityApp.Controllers
 {
@@ -36,5 +37,32 @@ namespace IdentityApp.Controllers
             }
             return View(userList);
         }
+
+        [HttpGet]
+        public IActionResult Edit(string userId)
+        {
+            var user = _dbContext.AppUser.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRole = _dbContext.UserRoles.ToList();
+            var roles = _dbContext.Roles.ToList();
+            var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+            if (role != null)
+            {
+                user.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+
+            user.RoleList = _dbContext.Roles.Select(u => new SelectListItem()
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+
+            return View(user);
+        }
+
     }
 }

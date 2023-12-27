7ce22e81d50af251bccde6cf8c11835d6f0e13a2
Vitalii <seminenkovs@gmail.com>
2023-12-27 02:09:37 +0100

    Created UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using IdentityApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

3f9c4e061c2a71b9308efb76b9c36acbf45d11fa
Vitalii <seminenkovs@gmail.com>
2023-12-29 22:09:00 +0100

    RoleController broth context filds

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityApp.Data;
+using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Controllers
 {
     public class RoleController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(ApplicationDbContext dbContext, UserManager<AppUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
             return View();

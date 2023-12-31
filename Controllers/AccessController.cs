9ed8cbb5cabc5bdb83fdf8c5a58a15def7914308
Vitalii <seminenkovs@gmail.com>
2023-12-31 15:11:04 +0100

    Created AccessController

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
